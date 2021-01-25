f8ca12d0cf21a971517fe0dc4b3349619762b1f5
BeatEngine <32369318+BeatEngine@users.noreply.github.com>
2021-01-25 21:07:19 +0100

    Version 2.0
    Pins:
    GND (GND, GND, GND, 2)
    VCC (VCC 5V, 4 3.3V, 11 5V)
    Motors([5, 6] | [9, 10])
    A0 -> Infrared front
    A1 -> Infrared right side (bottom between the chains)
    Acceleration X, Y, Z --> A3, A4, A5

--- mower_ino.ino
+++ mower_ino.ino
@@ -12,6 +12,13 @@ bool haspower = true;
 #define D7 13
 #define D8 15
 
+#define inpX A3
+#define inpY A4
+#define inpZ A5
+
+bool updater();
+
+
 bool drive(double x, double y, double speedR, double speedL, double duration, bool beginning)
 {
   double f = 1.0;
@@ -26,8 +33,8 @@ bool drive(double x, double y, double speedR, double speedL, double duration, bo
    
    while(millis()-start < duration)
    {
-    analogWrite(10, (int)(255)*speedL*(1 / (1 + f*exp(-0.01*(millis()-start)))));
-    analogWrite(5, (int)(255)*speedR*(1 / (1 + f*exp(-0.01*(millis()-start)))));
+    analogWrite(10, (int)(255)*speedL*(1 / (1 + f*exp(-0.005*(millis()-start)))));
+    analogWrite(5, (int)(255)*speedR*(1 / (1 + f*exp(-0.005*(millis()-start)))));
    }
    digitalWrite(10, LOW); 
    digitalWrite(5, LOW);
@@ -35,10 +42,10 @@ bool drive(double x, double y, double speedR, double speedL, double duration, bo
   else if(y >= 0.0)
   {
    unsigned long start = millis();
-   while(millis()-start < duration && analogRead(A0) > 400)
+   while(millis()-start < duration && analogRead(A0) > 400 && updater())
    {
-     analogWrite(9, (int)(255)*speedL*(1 / (1 + f*exp(-0.01*(millis()-start)))));
-     analogWrite(6, (int)(255)*speedR*(1 / (1 + f*exp(-0.01*(millis()-start)))));
+     analogWrite(9, (int)(255)*speedL*(1 / (1 + f*exp(-0.005*(millis()-start)))));
+     analogWrite(6, (int)(255)*speedR*(1 / (1 + f*exp(-0.005*(millis()-start)))));
    }
    digitalWrite(9, LOW); 
    digitalWrite(6, LOW);
@@ -50,8 +57,8 @@ bool drive(double x, double y, double speedR, double speedL, double duration, bo
     
     while(millis()-start < duration)
     {
-    analogWrite(6, (int)(255)*(1 / (1 + f*exp(-0.01*(millis()-start)))));
-    analogWrite(10, (int)(255)*(1 / (1 + f*exp(-0.01*(millis()-start)))));
+    analogWrite(6, (int)(255)*(1 / (1 + f*exp(-0.005*(millis()-start)))));
+    analogWrite(10, (int)(255)*(1 / (1 + f*exp(-0.005*(millis()-start)))));
     }
     digitalWrite(6, LOW); 
     digitalWrite(10, LOW);
@@ -61,16 +68,16 @@ bool drive(double x, double y, double speedR, double speedL, double duration, bo
     unsigned long start = millis();
     while(millis()-start < duration)
     {
-     analogWrite(9, (int)(255)*(1 / (1 + f*exp(-0.01*(millis()-start)))));
-     analogWrite(5, (int)(255)*(1 / (1 + f*exp(-0.01*(millis()-start)))));
+     analogWrite(9, (int)(255)*(1 / (1 + f*exp(-0.005*(millis()-start)))));
+     analogWrite(5, (int)(255)*(1 / (1 + f*exp(-0.005*(millis()-start)))));
     }
     digitalWrite(9, LOW); 
     digitalWrite(5, LOW);
   }
-   return analogRead(A0) < 401;
+   return analogRead(A0) < 401 || !updater();
 }
 
-
+long timeBegin;
 void setup()
 {
   pinMode(5, OUTPUT);
@@ -80,16 +87,127 @@ void setup()
 
   pinMode(11, OUTPUT);
   digitalWrite(11, HIGH);
+  pinMode(A0, INPUT);
   analogRead(A0);
-  //Serial.begin(9600);
+
+  //Acceleration sensors
+  pinMode(2, OUTPUT); //GND
+  pinMode(4, OUTPUT); //VCC
+  analogWrite(2, 0); //GND
+  analogWrite(4, 3.3/5.0*255); //VCC 3.3
+  pinMode(inpX, INPUT);
+  pinMode(inpY, INPUT);
+  pinMode(inpZ, INPUT);
+  analogRead(inpX);
+  analogRead(inpY);
+  analogRead(inpZ);
+  delay(800);
+  //Serial.begin(115200);
+  timeBegin = 0;
+  //while(!updater());
 }
 int counter = 0;
+
+double posX = 0;
+double posY = 0;
+double posZ = 0;
+
+double ax = 0.0;
+double ay = 0.0;
+double az = 0.0;
+
+double vx = 0.0;
+double vy = 0.0;
+double vz = 0.0;
+#define SENSORCONSTANT_X 364
+#define SENSORCONSTANT_Y 360
+#define SENSORCONSTANT_Z 433
+#define positive(vx) sqrt(vx*vx)
+#define threshold 1.1
+
+double acceleration(int pin)
+{
+  int value = 0;
+  for(int i = 0; i < 2; i++)
+  {
+    value += analogRead(pin);
+  }
+  return (((double)(value))/2-357)/75.83*9.81;
+}
+
+double filteredSpeed(double cmPerSecond)
+{
+  if(positive(cmPerSecond) < 0.01)
+  {
+    return 0.0;
+  }
+  return cmPerSecond;
+}
+
+//double middle = 0;
+//long cnm = 1;
+
+/** Returns true if not crashed! */
+bool updater()
+{
+  ax = acceleration(inpX);
+  ay = acceleration(inpY)-0.47;
+  az = acceleration(inpZ);
+  /*Serial.print(ax);
+  Serial.print(",");
+  Serial.print(ay);
+  Serial.print(",");
+  Serial.println(az);*/
+  return positive(ay) >= 5.0;
+}
+
 void loop()
 {
-  // A1 11 cm                 vb                       
+  // A1 11 cm                                    
   float L = 0.8;
   float R = 0.8;
-  //Serial.println(analogRead(A0));
+
+  /*if(timeBegin == 0)
+  {
+    timeBegin = millis();
+    ax1 = acceleration(inpX);
+    ay1 = acceleration(inpY);
+    az1 = acceleration(inpZ);
+  }
+  else
+  {
+    double relativeTime = (double)(millis()-timeBegin);
+    double ax2, ay2, az2;
+    ax2 = acceleration(inpX);
+    ay2 = acceleration(inpY);
+    az2 = acceleration(inpZ);
+    vx = relativeTime*0.5*(ax1+ax2); //cm per second
+    vy = relativeTime*0.5*(ay1+ay2);
+    //vz = filteredSpeed(relativeTime*0.5*(az1+az2));
+    //posX += vx * relativeTime;
+    posY += vy * relativeTime;
+    //posZ += vz * relativeTime;
+    
+    
+    Serial.print((ay1-ay2));
+    Serial.print(",");
+    Serial.print(vy);
+    Serial.print(",");
+    Serial.println(posY/10);
+    //Serial.print(",");
+    //middle += acceleration(inpZ);
+    //Serial.println(middle/(double)cnm);
+    //cnm++;
+    timeBegin = 0;
+  }*/
+
+  
+
+  
+
+  //posX += (double)(analogRead(inpX))/1024.0*relativeTime*relativeTime;
+  //posY += (double)(analogRead(inpY))/1024.0*relativeTime*relativeTime;
+  //posZ += (double)(analogRead(inpZ))/1024.0*relativeTime*relativeTime;
   if(analogRead(A1) < 401)
   {
     R = 0.75;
@@ -119,9 +237,12 @@ void loop()
       drive(0.0, -1.0, R, L, 400, cnt < 1);
       delay(200);
     }
-    if(cnt < 1)
+    else
     {
-      cnt ++;
+      if(cnt < 1)
+      {
+        cnt ++;
+      }
     }
   }
     
